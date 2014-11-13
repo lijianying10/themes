7f30b2c570991e0890113561fece6e0f27dc4cbb
Samy Pessé <samypesse@gmail.com>
2014-11-13 22:58:49 +0100

    Add base for default theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,19 @@ module.exports = function (grunt) {
     // Load NPM tasks
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks("grunt-bower-install-simple");
     grunt.loadNpmTasks('grunt-gh-pages');
 
     // Init GRUNT configuraton
     grunt.initConfig({
         'pkg': grunt.file.readJSON('package.json'),
+        'bower-install-simple': {
+            options: {
+                color:       true,
+                production:  false,
+                directory:   "src/vendors"
+            }
+        },
         'less': {
             development: {
                 options: {
@@ -42,6 +50,7 @@ module.exports = function (grunt) {
     });
 
     grunt.registerTask('build', [
+        'bower-install-simple',
         'less'
     ]);
 
